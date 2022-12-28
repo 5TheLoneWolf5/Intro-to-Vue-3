16547a1fed66c443e916635f764a1462024ca7e6
5TheLoneWolf5 <89516641+5TheLoneWolf5@users.noreply.github.com>
2022-12-28 12:46:05 -0300

    Personal File

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({ // Options object. At least an empty object.
+    data: function() {
+        return {
+            product: "Socks",
+            description: "Socks are great for wearing!"
+        }
+    }
+})
