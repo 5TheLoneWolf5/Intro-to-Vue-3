49188ad663f9c77c68dabf8b1a305f4b9407bd70
5TheLoneWolf5 <89516641+5TheLoneWolf5@users.noreply.github.com>
2023-01-16 10:23:10 -0300

    New exercise.

--- main.js
+++ main.js
@@ -4,21 +4,38 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: true, name: "Green Socks" },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false, name: "Blue Socks" },
             ]
         }
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.cart += 1;
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index;
+            console.log(index);
         }
+    },
+    computed: {
+        // Boosts perfomance.
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity;
+        },
+        onSale() {
+            let selectingVariant = this.variants[this.selectedVariant];
+            return selectingVariant.onSale ? `${selectingVariant.name} (${this.brand} ${this.product}) is on sale.` : ''
+        }
+        
     }
 })
