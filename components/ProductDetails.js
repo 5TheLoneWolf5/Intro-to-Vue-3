796957b76daff0f26aee97c766864d72599a50d6
5TheLoneWolf5 <89516641+5TheLoneWolf5@users.noreply.github.com>
2023-01-19 11:42:49 -0300

    New.

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -1,8 +1,14 @@
-app.component('product-display', {
+app.component('product-details', {
     props: {
         details: {
             type: Array,
             required: true
         }
-    }
+    },
+    template:
+    
+        `<ul>
+        <li v-for="detail in details">{{ detail }}</li>
+        </ul>`
+    
 })
